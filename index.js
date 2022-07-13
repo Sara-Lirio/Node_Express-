fd12cf0d8c5bd20d7604016c2d490342e33303da
Sara <saraeliza.lirio@gmail.com>
2022-07-13 12:29:41 -0300

    Criando as funções em loop

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
-const prompt = require('prompt-sync')()
+const prompt = require ('prompt-sync')({
+  history: require('prompt-sync-history')(),
+  sigint: false
+});
+
+function backgroundCSS() {
+  let backgroundColor = prompt('Background-color:');
+  let arraybackcolor = []
+
+  while (backgroundColor != 'sair' && backgroundColor != 'Sair' && backgroundColor != 'SAIR') {
+    arraybackcolor.push(backgroundColor)
+    backgroundColor = prompt('Background-color:')
+  }
+  let array1 = arraybackcolor.sort();
+  return ('Background-color: '+array1)
+}
+
+function fontsizeCSS() {
+  let fontSize = prompt('Font-size:');
+  let arrayfontsize = []
+
+  while (fontSize != 'sair' && fontSize != 'Sair' && fontSize != 'SAIR') {
+    arrayfontsize.push(fontSize)
+    fontSize = prompt('Font-size:')
+  }
+  let array2 = arrayfontsize.sort();
+  return ('Font-size: '+array2)
+}
+
+function textalignCSS() {
+  let textAlign = prompt('Text-align: ')
+  let arraytext = []
+
+  while (textAlign != 'sair' && textAlign != 'Sair' && textAlign != 'SAIR') {
+    arraytext.push(textAlign)
+    textAlign = prompt('Text-align:')
+  }
+  let array3 = arraytext.sort();
+  return ('Text-align:'+array3)
+}
+
+prompt.history.save()
+
+console.log(backgroundCSS()); 
+console.log(fontsizeCSS()); 
+console.log(textalignCSS());
+
+
 
